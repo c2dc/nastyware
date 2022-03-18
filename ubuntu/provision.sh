b231d6a27c006e99411651bc41a72d7437f8cf53
nilsonsangy <nilson.sangy@gmail.com>
2022-03-17 20:53:06 -0400

    DAMICORE files uploaded.

--- ubuntu/provision.sh
+++ ubuntu/provision.sh
@@ -8,3 +8,17 @@ apt install -y ssh
 
 service ssh enable
 
+
+
+### Configure python 2 virtual environment
+apt-get update
+apt-get install python2 curl
+cd /tmp
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+python2 get-pip.py
+pip2 install virtualenv
+mkdir ~/virtualenv
+cd ~/virtualenv
+virtualenv damicore
+source virtualenv/damicore/bin/activate
+pip install figtree
